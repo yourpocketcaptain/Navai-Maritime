3815966d08eba8593ce7d8bf5f6a78342995bae7
Arnau <arnau@navaitech.com>
2026-02-02 21:02:30 +0100

    fix: move firebase config to env vars

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "navai-151f5",
-  appId: "1:47292512558:web:3be70819759764e7c322bf",
-  storageBucket: "navai-151f5.firebasestorage.app",
-  apiKey: "AIzaSyAqmJbqwTSND0ElBoT-y7Qea1lg0Adcbio",
-  authDomain: "navai-151f5.firebaseapp.com",
-  messagingSenderId: "47292512558",
-  measurementId: "G-MJQ47SVWET",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase for SSR compatibility
